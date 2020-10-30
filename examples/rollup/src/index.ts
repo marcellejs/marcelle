448be5265c455313466b3279c8a48ac4de4ee44b
Jules Françoise <me@julesfrancoise.com>
2020-10-30 16:51:47 +0100

    Update rollup example

--- examples/rollup/src/index.ts
+++ examples/rollup/src/index.ts
@@ -2,48 +2,52 @@ import {
   browser,
   webcam,
   mobilenet,
-  capture,
-  createStream,
   dataset,
   button,
   parameters,
   progress,
   toggle,
-  text,
   mlp,
   createBackend,
   createDashboard,
-  createWizard,
-  MLPResults,
+  textfield,
+  trainingPlot,
+  batchPrediction,
+  confusion,
+  predictionPlot,
+  account,
+  Prediction,
   Stream,
   Instance,
-} from 'marcellejs';
-import { awaitPromises, map, skipRepeats } from '@most/core';
+} from '@marcellejs/core';
 
 // -----------------------------------------------------------
-// INPUT PIPELINE & CAPTURE TO DATASET
+// INPUT PIPELINE & DATA CAPTURE
 // -----------------------------------------------------------
 
-const w = webcam();
-const m = mobilenet();
+const input = webcam();
+const featureExtractor = mobilenet();
 
-const cap = capture({ input: w.$images, thumbnail: w.$thumbnails });
-const instances = createStream(
-  awaitPromises(
-    map(
-      async (instance: Instance): Promise<Instance> => ({
-        ...instance,
-        type: 'image',
-        features: await m.process(instance.data as ImageData),
-      }),
-      cap.$instances,
-    ),
-  ),
-);
+const label = textfield();
+label.name = 'Instance label';
+const capture = button({ text: 'Hold to record instances' });
+capture.name = 'Capture instances to the training set';
 
+const instances = input.$images
+  .filter(() => capture.$down.value)
+  .map(async (img: ImageData) => ({
+    type: 'image',
+    data: img,
+    label: label.$text.value,
+    thumbnail: input.$thumbnails.value,
+    features: await featureExtractor.process(img),
+  }))
+  .awaitPromises();
+
+// const backend = createBackend({ location: 'http://localhost:3030/', auth: true });
 const backend = createBackend({ location: 'localStorage' });
 const trainingSet = dataset({ name: 'TrainingSet', backend });
-trainingSet.capture(instances);
+trainingSet.capture(instances as Stream<Instance>);
 
 const trainingSetBrowser = browser(trainingSet);
 
@@ -52,137 +56,56 @@ const trainingSetBrowser = browser(trainingSet);
 // -----------------------------------------------------------
 
 const b = button({ text: 'Train' });
-const classifier = mlp({ layers: [128, 64], epochs: 30 });
+b.name = 'Training Launcher';
+const classifier = mlp({ layers: [64, 32], epochs: 20 });
 b.$click.subscribe(() => classifier.train(trainingSet));
 
 const params = parameters(classifier);
 const prog = progress(classifier);
+const plotTraining = trainingPlot(classifier);
 
 // -----------------------------------------------------------
-// PREDICTION
+// BATCH PREDICTION
+// -----------------------------------------------------------
+
+const batchMLP = batchPrediction({ name: 'mlp', backend });
+const confusionMatrix = confusion(batchMLP);
+
+const predictButton = button({ text: 'Update predictions' });
+predictButton.$click.subscribe(async () => {
+  await batchMLP.clear();
+  await batchMLP.predict(classifier, trainingSet);
+});
+
+// -----------------------------------------------------------
+// REAL-TIME PREDICTION
 // -----------------------------------------------------------
 
 const tog = toggle({ text: 'toggle prediction' });
-const results = text({ text: 'waiting for predictions...' });
 
-// DOM Stuff for the app
-const d = document.querySelector('#results');
-const resultImg: HTMLImageElement = document.querySelector('#result-img');
+const predictionStream = input.$images
+  .filter(() => tog.$checked.value)
+  .map(async (img: ImageData) => classifier.predict(await featureExtractor.process(img)))
+  .awaitPromises();
 
-// eslint-disable-next-line @typescript-eslint/no-empty-function
-let predictions: Stream<MLPResults>;
-createStream(skipRepeats(tog.$checked)).subscribe((x: boolean) => {
-  if (x) {
-    predictions = createStream(
-      awaitPromises(
-        map(async (img) => classifier.predict((await m.process(img)) as number[][]), w.$images),
-      ),
-    );
-    let PrevLabel = '';
-    predictions.subscribe((y: MLPResults) => {
-      results.$text.set(
-        `<h2>predicted label: ${y.label}</h2><p>Confidences: ${Object.values(
-          y.confidences,
-        ).map((z: number) => z.toFixed(2))}</p>`,
-      );
-      if (y.label !== PrevLabel) {
-        d.innerHTML = `predicted label: ${y.label}`;
-        resultImg.src =
-          y.label === 'A'
-            ? 'https://media.giphy.com/media/zlVf2eSgXIFFuTnEhz/giphy.gif'
-            : 'https://media.giphy.com/media/Oc8lIQHZsXqDu/giphy.gif';
-        PrevLabel = y.label;
-      }
-    });
-  } else {
-    predictions?.stop();
-  }
-});
+const plotResults = predictionPlot(predictionStream as Stream<Prediction>);
 
 // -----------------------------------------------------------
 // DASHBOARDS
 // -----------------------------------------------------------
 
-const app = createDashboard({
-  title: 'Marcelle Starter',
+const dashboard = createDashboard({
+  title: 'Marcelle Example - Dashboard',
   author: 'Marcelle Pirates Crew',
-  datasets: [trainingSet],
 });
 
-app.page('Data Management').useLeft(w, m).use(cap, trainingSetBrowser);
-app.page('Training').use(params, b, prog);
-app.page('Real-time prediction').useLeft(w).use(tog, results);
+dashboard
+  .page('Data Management')
+  .useLeft(input, featureExtractor)
+  .use([label, capture], trainingSetBrowser);
+dashboard.page('Training').use(params, b, prog, plotTraining);
+dashboard.page('Batch Prediction').use(predictButton, confusionMatrix);
+dashboard.page('Real-time Prediction').useLeft(input).use(tog, plotResults);
+dashboard.settings.useLeft(account(backend)).use(trainingSet);
 
-// -----------------------------------------------------------
-// WIZARD
-// -----------------------------------------------------------
-
-const bbb = button({ text: 'Record Examples (class a)' });
-const ttt = text({ text: 'Waiting for examples...' });
-bbb.$down.subscribe((x) => {
-  cap.$capturing.set(x);
-});
-trainingSet.$countPerClass.subscribe((c) => {
-  const label = cap.$label.value;
-  const numExamples = c[label];
-  ttt.$text.set(
-    numExamples ? `Recorded ${numExamples} examples of "${label}"` : 'Waiting for examples...',
-  );
-});
-
-const wizard = createWizard();
-
-wizard
-  .step()
-  .title('Record examples for class A')
-  .description('Ich bin ein description')
-  .use(w, bbb, ttt)
-  .step()
-  .title('Record examples for class B')
-  .description('Ich bin ein description')
-  .use(w, bbb, ttt)
-  .step()
-  .title('Train the model')
-  .description('Ich bin ein MLP')
-  .use(b, prog)
-  .step()
-  .title('Test the classifier')
-  .description('Ich bin ein classifier')
-  .use(w, tog, results);
-
-function configureWizard(label: string) {
-  cap.$label.set(label);
-  bbb.$text.set(`Record Examples (class ${label})`);
-  const numExamples = trainingSet.$countPerClass.value[label];
-  ttt.$text.set(
-    numExamples ? `Recorded ${numExamples} examples of "${label}"` : 'Waiting for examples...',
-  );
-}
-
-wizard.$current.subscribe((stepIndex) => {
-  if (stepIndex === 0) {
-    configureWizard('A');
-  } else if (stepIndex === 1) {
-    configureWizard('B');
-  }
-});
-
-// -----------------------------------------------------------
-// MAIN APP STUFF
-// -----------------------------------------------------------
-
-w.$mediastream.subscribe((s) => {
-  (document.querySelector('#my-webcam') as HTMLVideoElement).srcObject = s;
-});
-
-setTimeout(() => {
-  w.$active.set(true);
-}, 200);
-
-document.querySelector('#start-wizard').addEventListener('click', () => {
-  wizard.start();
-});
-
-document.querySelector('#start-dashboard').addEventListener('click', () => {
-  app.start();
-});
+dashboard.start();
