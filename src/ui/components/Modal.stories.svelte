9b94b88c666455d565c9e4d85c68e96ce87dfc7e
Jules Françoise <me@julesfrancoise.com>
2021-05-19 13:15:45 +0200

    Add a story for Modal UI component

---
+++ src/ui/components/Modal.stories.svelte
@@ -0,0 +1,26 @@
+<script>
+  import { Meta, Template, Story } from '@storybook/addon-svelte-csf';
+  import Button from './Button.svelte';
+  import Modal from './Modal.svelte';
+
+</script>
+
+<Meta
+  title="Design System/Modal"
+  component={Modal}
+  parameters={{ backgrounds: { default: 'dashboard' } }}
+/>
+
+<Story name="Default">
+  <Modal>
+    <div class="marcelle p-4">
+      <h3>Hello</h3>
+      <p>Here is a simple modal window with some buttons</p>
+      <div class="flex justify-end">
+        <Button>Cancel</Button>
+        <span class="w-1" />
+        <Button variant="filled">Confirm</Button>
+      </div>
+    </div>
+  </Modal>
+</Story>
