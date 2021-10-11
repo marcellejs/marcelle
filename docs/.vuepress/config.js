a98907d477d27c0eee721dcb972440012520dafc
Jules Françoise <me@julesfrancoise.com>
2021-10-11 21:47:07 +0200

    Update docs

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -22,7 +22,7 @@ module.exports = {
       },
       {
         text: 'Examples',
-        link: '/examples/',
+        link: 'https://demos.marcelle.dev/',
       },
       {
         text: 'Credits',
