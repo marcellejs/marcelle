d5424ddd579c58df171953c941455c028d6c63cc
Jules Françoise <me@julesfrancoise.com>
2022-04-03 19:45:54 +0200

    Remove deprecated CLI and Yeoman generator packages

--- packages/yeoman-generator/generators/component/templates/js/index.js
+++
@@ -1,5 +0,0 @@
-import { <%= className %> } from './<%= kebabName %>.component';
-
-export function <%= camelName %>(...args) {
-  return new <%= className %>(...args);
-}
--- packages/yeoman-generator/generators/component/templates/ts/index.ts
+++
@@ -1,7 +0,0 @@
-import { <%= className %> } from './<%= kebabName %>.component';
-
-export function <%= camelName %>(...args: ConstructorParameters<typeof <%= className %>>): <%= className %> {
-  return new <%= className %>(...args);
-}
-
-export type { <%= className %> };
