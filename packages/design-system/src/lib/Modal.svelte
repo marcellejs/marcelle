372b7be2621845481bbce33e6cf816311225f3b1
Jules Françoise <me@julesfrancoise.com>
2022-06-28 15:52:24 +0200

    Improve modal UI component

--- packages/design-system/src/lib/Modal.svelte
+++ packages/design-system/src/lib/Modal.svelte
@@ -1,47 +1,47 @@
 <script lang="ts">
-	import { createEventDispatcher } from 'svelte';
+  import { createEventDispatcher } from 'svelte';
 
-	const dispatch = createEventDispatcher();
-	export function quit(): void {
-		dispatch('quit');
-	}
+  const dispatch = createEventDispatcher();
+  export function quit(): void {
+    dispatch('quit');
+  }
 
-	function onOutsideClick() {
-		quit();
-	}
+  function onOutsideClick() {
+    quit();
+  }
 </script>
 
 <div class="modal-container">
-	<div class="overlay">
-		<div on:click={onOutsideClick} class="absolute inset-0 bg-gray-500 opacity-50" />
-	</div>
-	<div class="modal">
-		<slot />
-	</div>
+  <div class="overlay">
+    <div on:click={onOutsideClick} class="absolute inset-0 bg-gray-500 opacity-50" />
+  </div>
+  <div class="modal">
+    <slot />
+  </div>
 </div>
 
 <style type="text/postcss">
-	.modal-container {
-		@apply absolute min-h-screen top-0 inset-x-0 p-4 pb-4  z-20;
-	}
+  .modal-container {
+    @apply absolute min-h-screen top-0 inset-x-0 p-4 pb-4  z-20;
+  }
 
-	@screen sm {
-		.modal-container {
-			@apply flex items-center justify-center;
-		}
-	}
+  @screen sm {
+    .modal-container {
+      @apply flex items-center justify-center;
+    }
+  }
 
-	.overlay {
-		@apply absolute min-h-screen inset-0 transition-opacity;
-	}
+  .overlay {
+    @apply absolute min-h-screen inset-0 transition-opacity;
+  }
 
-	.modal {
-		@apply bg-white rounded-lg overflow-hidden shadow-xl transform transition-all;
-	}
+  .modal {
+    @apply bg-white rounded-lg shadow-xl transform transition-all;
+  }
 
-	@screen sm {
-		.modal {
-			@apply max-w-3xl w-full;
-		}
-	}
+  @screen sm {
+    .modal {
+      @apply max-w-3xl w-full;
+    }
+  }
 </style>
