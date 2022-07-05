4a13ca8d8916e91089c8afc37e91a078a9a32800
Jules Françoise <me@julesfrancoise.com>
2022-07-05 10:31:13 +0200

    use adapter-static in design system

--- packages/design-system/svelte.config.js
+++ packages/design-system/svelte.config.js
@@ -1,23 +1,23 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://github.com/sveltejs/svelte-preprocess
-	// for more information about preprocessors
-	preprocess: preprocess({ postcss: true }),
+  // Consult https://github.com/sveltejs/svelte-preprocess
+  // for more information about preprocessors
+  preprocess: preprocess({ postcss: true }),
 
-	kit: {
-		adapter: adapter(),
+  kit: {
+    adapter: adapter(),
 
-		// Override http methods in the Todo forms
-		methodOverride: {
-			allowed: ['PATCH', 'DELETE']
-		},
-		package: {
-			dir: 'dist'
-		}
-	}
+    // Override http methods in the Todo forms
+    methodOverride: {
+      allowed: ['PATCH', 'DELETE'],
+    },
+    package: {
+      dir: 'dist',
+    },
+  },
 };
 
 export default config;
